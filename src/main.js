33f7759a85278320585b3d81535aa0f478994eaf
Hawarikun <2006083@itc.ac.id>
2025-11-18 18:26:31 +0700

    update: enable lenis back

--- src/main.js
+++ src/main.js
@@ -10,36 +10,37 @@ import servicesAnimation from "./pages/services.js";
 import { loadRiveAnimation } from "./utils/rive.js";
 import initGlobalAnimations from "./utils/global.js";
 
-// let lenis = new Lenis({
-// 	lerp: 0.1,
-// 	wheelMultiplier: 0.7,
-// 	gestureOrientation: "vertical",
-// 	normalizeWheel: false,
-// 	smoothTouch: false,
-// });
-// function raf(time) {
-// 	lenis.raf(time);
-// 	requestAnimationFrame(raf);
-// }
+let lenis = new Lenis({
+	// lerp: 0.1,
+	// wheelMultiplier: 0.7,
+	// gestureOrientation: "vertical",
+	// normalizeWheel: false,
+	// smoothTouch: false,
+});
 
-// requestAnimationFrame(raf);
+function raf(time) {
+	lenis.raf(time);
+	requestAnimationFrame(raf);
+}
 
-// $("[data-lenis-start]").on("click", function () {
-// 	lenis.start();
-// });
+requestAnimationFrame(raf);
 
-// $("[data-lenis-stop]").on("click", function () {
-// 	lenis.stop();
-// });
+$("[data-lenis-start]").on("click", function () {
+	lenis.start();
+});
 
-// $("[data-lenis-toggle]").on("click", function () {
-// 	$(this).toggleClass("stop-scroll");
-// 	if ($(this).hasClass("stop-scroll")) {
-// 		lenis.stop();
-// 	} else {
-// 		lenis.start();
-// 	}
-// });
+$("[data-lenis-stop]").on("click", function () {
+	lenis.stop();
+});
+
+$("[data-lenis-toggle]").on("click", function () {
+	$(this).toggleClass("stop-scroll");
+	if ($(this).hasClass("stop-scroll")) {
+		lenis.stop();
+	} else {
+		lenis.start();
+	}
+});
 
 
 function initAnimations() {
